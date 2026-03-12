126badd6dc917b35a6c5687d4e9e9fc81cd91ba3
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2026-03-12 15:44:09 +0100

    Add completion support for the items in the library

--- share/completions/safer.bash
+++ share/completions/safer.bash
@@ -1,10 +1,26 @@
 _safer_complete() {
-    local cur
+    local cur prev lib_path subdir items dir opts
     cur="${COMP_WORDS[COMP_CWORD]}"
-
-    local opts="-e -p -k -n -d -R -X -K -S -f -s -l -i -v -h"
-
-    COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    lib_path=${SAFER_LIB_PATH:-~/.local/share/safer:/usr/share/safer:/usr/local/share/safer}
+    if [[ "$prev" == "-l" || "$prev" == "-i" ]]; then
+        [[ "$prev" == "-l" ]] && subdir=tools || subdir=images
+        items=()
+        IFS=':' read -ra dirs <<< "$lib_path"
+        for dir in "${dirs[@]}"; do
+            if [[ -d "$dir/$subdir" ]]; then
+                for f in "$dir/$subdir"/*; do
+                    if [[ -f "$f" ]]; then
+                        items+=("$(basename "$f")")
+                    fi
+                done
+            fi
+        done
+        COMPREPLY=( "${items[@]}" )
+    else
+        opts="-e -p -k -n -d -R -X -K -S -f -s -l -i -v -h"
+        COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+    fi
 }
 
 complete -F _safer_complete safer
